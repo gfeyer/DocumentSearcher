8bbaa21ee7e697f6f1840d9fa57d6e2b5e1bed7b
Gabriel Feyer <gabriel.feyer@indexexchange.com>
2021-05-18 15:21:50 -0400

    move index to internal namespace

--- src/lucene_api/index.cpp
+++ src/lucene_api/index.cpp
@@ -3,75 +3,79 @@
 
 namespace lucene_api {
 
-    int32_t docNumber = 0;
+    namespace internal {
 
-    std::string readFileIntoString(const std::wstring& path) {
-        auto ss = std::ostringstream{};
-        std::ifstream input_file(path);
-        if (!input_file.is_open()) {
-            std::cerr << "Could not open the file" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        ss << input_file.rdbuf();
-        return ss.str();
-    }
 
-    std::wstring readFileIntoWString(const std::wstring& path) {
-        auto ss = std::wostringstream{};
-        std::wifstream input_file(path, std::ios::binary);
-        if (!input_file.is_open()) {
-            std::cerr << "Could not open the file" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        ss << input_file.rdbuf();
-        return ss.str();
-    }
+        int32_t docNumber = 0;
 
-    Lucene::DocumentPtr fileDocument(const Lucene::String& docFile) {
-        using namespace Lucene;
-        DocumentPtr doc = newLucene<Document>();
-
-        // Add the path of the file as a field named "path".  Use a field that is indexed (ie. searchable), but
-        // don't tokenize the field into words.
-        doc->add(newLucene<Field>(L"path", docFile, Field::STORE_YES, Field::INDEX_NOT_ANALYZED));
-
-        // Add the last modified date of the file a field named "modified".  Use a field that is indexed (ie. searchable),
-        // but don't tokenize the field into words.
-        doc->add(newLucene<Field>(L"modified", DateTools::timeToString(FileUtils::fileModified(docFile), DateTools::RESOLUTION_MINUTE),
-            Field::STORE_YES, Field::INDEX_NOT_ANALYZED));
-
-        // Add the contents of the file to a field named "contents".  Specify a Reader, so that the text of the file is
-        // tokenized and indexed, but not stored.  Note that FileReader expects the file to be in the system's default
-        // encoding.  If that's not the case searching for special characters will fail.
-
-        auto myfile = readFileIntoString(docFile);
-
-        doc->add(newLucene<Field>(L"contents", utf8ToUtf16(myfile), Field::STORE_YES, Field::INDEX_ANALYZED));
-        //doc->add(newLucene<Field>(L"contents", newLucene<FileReader>(docFile)));
-
-        return doc;
-    }
-
-    void IndexDocsWithWriter(const Lucene::IndexWriterPtr& writer, const Lucene::String& sourceDir) {
-        using namespace Lucene;
-        
-        HashSet<String> dirList(HashSet<String>::newInstance());
-        if (!FileUtils::listDirectory(sourceDir, false, dirList)) {
-            return;
-        }
-
-        for (HashSet<String>::iterator dirFile = dirList.begin(); dirFile != dirList.end(); ++dirFile) {
-            String docFile(FileUtils::joinPath(sourceDir, *dirFile));
-            if (FileUtils::isDirectory(docFile)) {
-                IndexDocsWithWriter(writer, docFile);
+        std::string readFileIntoString(const std::wstring& path) {
+            auto ss = std::ostringstream{};
+            std::ifstream input_file(path);
+            if (!input_file.is_open()) {
+                std::cerr << "Could not open the file" << std::endl;
+                exit(EXIT_FAILURE);
             }
-            else {
-                std::wcout << L"Adding [" << ++docNumber << L"]: " << *dirFile << L"\n";
+            ss << input_file.rdbuf();
+            return ss.str();
+        }
 
-                try {
-                    writer->addDocument(fileDocument(docFile));
+        std::wstring readFileIntoWString(const std::wstring& path) {
+            auto ss = std::wostringstream{};
+            std::wifstream input_file(path, std::ios::binary);
+            if (!input_file.is_open()) {
+                std::cerr << "Could not open the file" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            ss << input_file.rdbuf();
+            return ss.str();
+        }
+
+        Lucene::DocumentPtr fileDocument(const Lucene::String& docFile) {
+            using namespace Lucene;
+            DocumentPtr doc = newLucene<Document>();
+
+            // Add the path of the file as a field named "path".  Use a field that is indexed (ie. searchable), but
+            // don't tokenize the field into words.
+            doc->add(newLucene<Field>(L"path", docFile, Field::STORE_YES, Field::INDEX_NOT_ANALYZED));
+
+            // Add the last modified date of the file a field named "modified".  Use a field that is indexed (ie. searchable),
+            // but don't tokenize the field into words.
+            doc->add(newLucene<Field>(L"modified", DateTools::timeToString(FileUtils::fileModified(docFile), DateTools::RESOLUTION_MINUTE),
+                Field::STORE_YES, Field::INDEX_NOT_ANALYZED));
+
+            // Add the contents of the file to a field named "contents".  Specify a Reader, so that the text of the file is
+            // tokenized and indexed, but not stored.  Note that FileReader expects the file to be in the system's default
+            // encoding.  If that's not the case searching for special characters will fail.
+
+            auto myfile = readFileIntoString(docFile);
+
+            doc->add(newLucene<Field>(L"contents", utf8ToUtf16(myfile), Field::STORE_YES, Field::INDEX_ANALYZED));
+            //doc->add(newLucene<Field>(L"contents", newLucene<FileReader>(docFile)));
+
+            return doc;
+        }
+
+        void IndexDocsWithWriter(const Lucene::IndexWriterPtr& writer, const Lucene::String& sourceDir) {
+            using namespace Lucene;
+
+            HashSet<String> dirList(HashSet<String>::newInstance());
+            if (!FileUtils::listDirectory(sourceDir, false, dirList)) {
+                return;
+            }
+
+            for (HashSet<String>::iterator dirFile = dirList.begin(); dirFile != dirList.end(); ++dirFile) {
+                String docFile(FileUtils::joinPath(sourceDir, *dirFile));
+                if (FileUtils::isDirectory(docFile)) {
+                    IndexDocsWithWriter(writer, docFile);
                 }
-                catch (FileNotFoundException&) {
+                else {
+                    std::wcout << L"Adding [" << ++docNumber << L"]: " << *dirFile << L"\n";
+
+                    try {
+                        writer->addDocument(fileDocument(docFile));
+                    }
+                    catch (FileNotFoundException&) {
+                    }
                 }
             }
         }
@@ -105,7 +109,7 @@ namespace lucene_api {
 
             std::wcout << L"Indexing to directory: " << indexDir << L"...\n";
 
-            IndexDocsWithWriter(writer, sourceDir);
+            internal::IndexDocsWithWriter(writer, sourceDir);
 
             uint64_t endIndex = MiscUtils::currentTimeMillis();
             uint64_t indexDuration = endIndex - beginIndex;
@@ -125,6 +129,10 @@ namespace lucene_api {
             std::wcout << L"Exception: " << e.getError() << L"\n";
             return 1;
         }
+        return 0;
+    }
+
+    int UpdateDocs(std::string source, std::string index) {
         return 0;
     }
 }
--- src/lucene_api/index.h
+++ src/lucene_api/index.h
@@ -15,12 +15,14 @@
 // TODO: cleanup references and use proper notation for lucene namespace
 
 namespace lucene_api {
-
-    std::string readFileIntoString(const std::wstring& path);
-    std::wstring readFileIntoWString(const std::wstring& path);
-    Lucene::DocumentPtr fileDocument(const Lucene::String& docFile);
-    void IndexDocsWithWriter(const Lucene::IndexWriterPtr& writer, const Lucene::String& sourceDir);
+    namespace internal {
+        std::string readFileIntoString(const std::wstring& path);
+        std::wstring readFileIntoWString(const std::wstring& path);
+        Lucene::DocumentPtr fileDocument(const Lucene::String& docFile);
+        void IndexDocsWithWriter(const Lucene::IndexWriterPtr& writer, const Lucene::String& sourceDir);
+    }
     int IndexDocs(std::string source, std::string index);
+    int UpdateDocs(std::string source, std::string index);
 }
 
 #endif  // INDEX_H_
