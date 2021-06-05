33e2bc2acaf4e2d423c117cf1b7c592c12f7982e
Gabriel Feyer <gabriel.feyer@indexexchange.com>
2021-06-05 14:01:06 -0400

    format user query

--- src/util/word_util.cpp
+++ src/util/word_util.cpp
@@ -4,27 +4,22 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <regex>
 
-std::vector<std::string> word_util::SplitIntoWords(std::string sentence)
+std::vector<std::string> word_util::SplitIntoWords(std::string user_query)
 {
-    
-    // Used to split string around spaces.
-    std::istringstream ss(sentence);
+    // Remove special characters
+    user_query = std::regex_replace(user_query, std::regex(R"(["*~])"), " ");
 
+    // remove keywords
+    user_query = std::regex_replace(user_query, std::regex("AND"), "");
+
+    // Used to split string around spaces
+    std::istringstream ss(user_query);
     std::string word; // for storing each word
-
     std::set<std::string> wordset;
-
     while (ss >> word)
     {
-        if (word == "AND") {
-            continue;
-        }
-
-        if (*word.rbegin() == '*') {
-            word.pop_back();
-        }
-
         wordset.insert(word);
     }
 
